c8166d604d99ae42e732bec0762d6c2e4b245145
Sean Zaferopolos <szafero@gmail.com>
2015-02-12 20:19:46 +0000

    Initial symlink operations added to init

--- init.sh
+++ init.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+#Tmux
+sudo rm ~/.tmux.conf
+ln tmux.conf ~/.tmux.conf
+tmux source-file ~/.tmux.conf
+
+#vimrc
+sudo rm ~/.vimrc.local
+ln vimrc.local ~/.vimrc.local
+
+#inputrc
+sudo rm ~/.inputrc
+ln inputrc ~/.inputrc
+source ~/.inputrc
